e7da832ea7c4b22763e2b38076b340aeb2466c4f
Tyler Slaton <tyslaton@redhat.com>
2021-11-17 17:06:27 -0500

    refactor(generate): update generate to export a struct and utilize an io.Reader interface

--- pkg/generate/template.go
+++ pkg/generate/template.go
@@ -1,31 +1,49 @@
 package generate
 
 import (
+	"errors"
+	"io"
+	"io/ioutil"
 	"regexp"
 	"strings"
+)
+
+var (
+	ErrCouldNotReadFile = errors.New("could not read file")
 )
 
 // Template contains an array of documents that can be
 // interacted with with its various functions.
 type template struct {
-	documents []string
+	documents          []string
+	processedDocuments []string
 }
 
-func newTemplate(file string) template {
+func buildTemplate(file io.Reader) (template, error) {
 	// Separate the documents by the yaml separator and build a template with them
-	docs := strings.Split(file, "---")
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return template{}, ErrCouldNotReadFile
+	}
 
-	constructedTemplate := template{}
-	constructedTemplate.documents = append(constructedTemplate.documents, docs...)
+	docs := strings.Split(string(fileBytes), "---")
 
-	return constructedTemplate
+	var constructedTemplate template
+	for _, doc := range docs {
+		trimmedDoc := strings.TrimSpace(doc)
+		if trimmedDoc != "" {
+			constructedTemplate.documents = append(constructedTemplate.documents, trimmedDoc)
+		}
+	}
+
+	return constructedTemplate, nil
 }
 
 // has determines if any of the documents for the template
 // contains the specified string.
-func (t *template) has(find string) bool {
-	for _, document := range t.documents {
-		if document == find {
+func (t *template) has(searchDocument string) bool {
+	for _, document := range t.processedDocuments {
+		if document == searchDocument {
 			return true
 		}
 	}
@@ -33,19 +51,17 @@ func (t *template) has(find string) bool {
 }
 
 // with builds the template documents with the combination set specified
-func (t *template) with(combo map[string]string, to template) template {
+func (t *template) with(combo map[string]string) {
 	// For each document in the template evaluate the current combination set
 	for _, doc := range t.documents {
-		incDoc := strings.TrimSpace(doc)
 		for key, val := range combo {
-			incDoc = regexp.MustCompile(key+`\b`).ReplaceAllString(incDoc, val)
+			doc = regexp.MustCompile(key+`\b`).ReplaceAllString(doc, val)
 		}
 
 		// Add the document if it isn't empty and doesn't already exist in the template
-		shouldAdd := incDoc != "" && !to.has(incDoc)
+		shouldAdd := doc != "" && !t.has(doc)
 		if shouldAdd {
-			to.documents = append(to.documents, incDoc)
+			t.processedDocuments = append(t.processedDocuments, doc)
 		}
 	}
-	return to
 }
