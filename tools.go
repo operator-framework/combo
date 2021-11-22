b1997aa684b160c7dc79271455176c8ed7afe06f
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-22 13:41:14 +0800

    Delete tools.go

--- tools.go
+++
@@ -1,9 +0,0 @@
-//go:build tools
-// +build tools
-
-package tools
-
-import (
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint" // Better linting
-	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"     // Generate deepcopy, conversion, and CRDs
-)
