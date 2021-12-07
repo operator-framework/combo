9e016a7d51d9af345b9d88dff9a8d1f8fec54ad7
Tyler Slaton <tyslaton@redhat.com>
2021-12-07 11:42:44 -0500

    test(e2e): add test case for healthy and unhealthy input

--- go.mod
+++ go.mod
@@ -11,9 +11,9 @@ require (
 	github.com/spf13/cobra v1.2.1
 	github.com/stretchr/testify v1.7.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+	k8s.io/api v0.22.4
 	k8s.io/apimachinery v0.22.4
 	k8s.io/client-go v0.22.4
-	k8s.io/kubectl v0.22.4
 	sigs.k8s.io/controller-runtime v0.10.2
 	sigs.k8s.io/controller-tools v0.7.0
 )
@@ -173,7 +173,6 @@ require (
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	honnef.co/go/tools v0.2.1 // indirect
-	k8s.io/api v0.22.4 // indirect
 	k8s.io/apiextensions-apiserver v0.22.2 // indirect
 	k8s.io/component-base v0.22.4 // indirect
 	k8s.io/klog/v2 v2.9.0 // indirect
---
+++ test/e2e/controller_test.go
@@ -0,0 +1,150 @@
+package e2e
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/operator-framework/combo/api/v1alpha1"
+	comboConditions "github.com/operator-framework/combo/pkg/conditions"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+var _ = Describe("Combination controller", func() {
+	validTemplateCR := v1alpha1.Template{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: "validtemplate",
+		},
+		Spec: v1alpha1.TemplateSpec{
+			Body:       "---\nFIRSTNAME: LASTNAME",
+			Parameters: []string{"FIRSTNAME, LASTNAME"},
+		},
+	}
+
+	validCombinationCR := v1alpha1.Combination{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: "validcombination",
+		},
+		Spec: v1alpha1.CombinationSpec{
+			Arguments: []v1alpha1.Argument{
+				{
+					Key: "FIRSTNAME",
+					Values: []string{
+						"John",
+						"Luke",
+					},
+				},
+				{
+					Key: "LASTNAME",
+					Values: []string{
+						"Snow",
+						"Skywalker",
+					},
+				},
+			},
+		},
+	}
+
+	expectedEvaluations := []string{
+		"John: Snow",
+		"John: Skywalker",
+		"Luke: Snow",
+		"Luke: Skywalker",
+	}
+
+	When("given healthy input and a healthy template", func() {
+		var ctx context.Context
+		var validTemplateCRCopy *v1alpha1.Template
+		var validCombinationCRCopy *v1alpha1.Combination
+
+		BeforeEach(func() {
+			ctx = context.Background()
+
+			// Create copies of valid CR's
+			validTemplateCRCopy = validTemplateCR.DeepCopy()
+			validCombinationCRCopy = validCombinationCR.DeepCopy()
+
+			// Create and defer deletion of a valid template
+			err := kubeclient.Create(ctx, validTemplateCRCopy)
+			if err != nil {
+				Fail("Failed to create validTemplateCR: " + err.Error())
+			}
+
+			// Create and defer deletion of a valid combination
+			validCombinationCRCopy.Spec.Template = validTemplateCRCopy.Name
+			err = kubeclient.Create(ctx, validCombinationCRCopy)
+			if err != nil {
+				Fail("Failed to create validCombinationCR: " + err.Error())
+			}
+		})
+
+		AfterEach(func() {
+			kubeclient.Delete(ctx, validTemplateCRCopy)
+			kubeclient.Delete(ctx, validCombinationCRCopy)
+			ctx.Done()
+		})
+
+		It("should get the correct evaluations and a Processed status", func() {
+			// Give up to a minute (default eventually timeout) for the combination to process properly
+			Eventually(func(g Gomega) error {
+				combinationQuery := types.NamespacedName{Name: validCombinationCRCopy.Name}
+
+				var retrievedCombination v1alpha1.Combination
+				err := kubeclient.Get(ctx, combinationQuery, &retrievedCombination)
+
+				var conditionReasons []string
+				for _, condition := range retrievedCombination.Status.Conditions {
+					conditionReasons = append(conditionReasons, condition.Reason)
+				}
+
+				g.Expect(conditionReasons).To(ContainElement(comboConditions.ProccessedCombinationsCondition.Reason))
+				g.Expect(retrievedCombination.Status.Evaluation).To(ContainElements(expectedEvaluations))
+
+				return err
+			}).Should(BeZero())
+		})
+
+	})
+
+	When("given healthy input and an non-existant template", func() {
+		var ctx context.Context
+		var validCombinationCRCopy *v1alpha1.Combination
+
+		BeforeEach(func() {
+			ctx = context.Background()
+
+			// Create copies of valid CR
+			validCombinationCRCopy = validCombinationCR.DeepCopy()
+
+			// Create and defer deletion of a valid combination
+			validCombinationCRCopy.Spec.Template = "doesnotexist"
+			err := kubeclient.Create(ctx, validCombinationCRCopy)
+			if err != nil {
+				Fail("Failed to create validCombinationCR: " + err.Error())
+			}
+		})
+
+		AfterEach(func() {
+			kubeclient.Delete(ctx, validCombinationCRCopy)
+			ctx.Done()
+		})
+
+		It("should fail and output a TemplateNotFound status", func() {
+			// Give up to a minute (default eventually timeout) for the combination to process properly
+			Eventually(func() ([]string, error) {
+				combinationQuery := types.NamespacedName{Name: validCombinationCRCopy.Name}
+
+				var retrievedCombination v1alpha1.Combination
+				err := kubeclient.Get(ctx, combinationQuery, &retrievedCombination)
+
+				var conditionReasons []string
+				for _, condition := range retrievedCombination.Status.Conditions {
+					conditionReasons = append(conditionReasons, condition.Reason)
+				}
+
+				return conditionReasons, err
+			}).Should(ContainElement(comboConditions.TemplateNotFoundCondition.Reason))
+		})
+	})
+})
